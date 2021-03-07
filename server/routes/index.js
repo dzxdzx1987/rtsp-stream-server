02b08b102566e38eca3da5a661d6f7901bd93083
Kevin <dzxdzx1987@gmail.com>
2021-03-07 20:04:00 +0900

    [chg]Fixed null exception about stream obj.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,32 +3,7 @@ var router = express.Router();
 var Stream = require('node-rtsp-stream');
 var streamObj = null;
 
-/*var streamObj = new Stream({
-  name: 'name',
-  streamUrl: 'rtsp://nextk.synology.me/vod/fa_test',
-  wsPort: 9999,
-  ffmpegOptions: { // options ffmpeg flags
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30 // options with required values specify the value after the key
-  }
-});
-
-streamObj.on('exitWithError', () => {
-  streamObj.stop();
-});*/
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  streamObj.startMpeg1Stream();
-  res.render('index', { title: 'RTSP Stream Demo' });
-});
-
-router.get('/manage', function(req, res, next) {
-  res.render('management', { title: 'RTSP Stream Demo' });
-});
-
-router.get('/playStream', function(req, res, next) {
+var initStreamObj = function () {
   streamObj = new Stream({
     name: 'name',
     streamUrl: 'rtsp://nextk.synology.me/vod/fa_test',
@@ -38,11 +13,32 @@ router.get('/playStream', function(req, res, next) {
       '-r': 30 // options with required values specify the value after the key
     }
   });
-  
+
   streamObj.on('exitWithError', () => {
     streamObj.stop();
   });
+}
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if (streamObj == null) {
+    initStreamObj();
+  } else {
+    streamObj.startMpeg1Stream();
+  }
   
+  res.render('index', { title: 'RTSP Stream Demo' });
+});
+
+router.get('/manage', function(req, res, next) {
+  res.render('management', { title: 'RTSP Stream Demo' });
+});
+
+router.get('/playStream', function(req, res, next) {
+  if (streamObj == null) {
+    initStreamObj();
+  }
   res.write('OK');
 });
 
